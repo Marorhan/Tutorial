a9a3994f6e5021e075232abb54f7da7c24bd8ef3
Benyo <bengtake.g.b.olsson@hotmail.com>
2025-09-24 23:27:24 +0200

    Added datagen

--- src/main/java/net/bengto/tutorialmod/item/ModCreativeModTabs.java
+++ src/main/java/net/bengto/tutorialmod/item/ModCreativeModTabs.java
@@ -1,17 +1,18 @@
 package net.bengto.tutorialmod.item;
 
-import net.bengto.tutorialmod.blocks.ModBlocks;
 import net.bengto.tutorialmod.TutorialMod;
+import net.bengto.tutorialmod.block.ModBlocks;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModCreativeModTabs {
-    public  static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TutorialMod.MOD_ID);
 
     public static final RegistryObject<CreativeModeTab> TUTORIAL_TAB = CREATIVE_MODE_TABS.register("tutorial_tab",
@@ -21,13 +22,26 @@ public class ModCreativeModTabs {
                         pOutput.accept(ModItems.SAPPHIRE.get());
                         pOutput.accept(ModItems.RAW_SAPPHIRE.get());
 
+                        pOutput.accept(ModItems.METAL_DETECTOR.get());
+
+                        pOutput.accept(Items.DIAMOND);
+                        pOutput.accept(ModItems.STRAWBERRY.get());
+                        pOutput.accept(ModItems.PINE_CONE.get());
+
                         pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
                         pOutput.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
+
+                        pOutput.accept(ModBlocks.SAPPHIRE_ORE.get());
+                        pOutput.accept(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
+                        pOutput.accept(ModBlocks.NETHER_SAPPHIRE_ORE.get());
+                        pOutput.accept(ModBlocks.END_STONE_SAPPHIRE_ORE.get());
+                        pOutput.accept(ModBlocks.SOUND_BLOCK.get());
+
                     })
                     .build());
 
 
-    public  static void  register(IEventBus eventBus) {
+    public static void register(IEventBus eventBus) {
         CREATIVE_MODE_TABS.register(eventBus);
     }
 }
--- src/main/java/net/bengto/tutorialmod/item/ModItems.java
+++ src/main/java/net/bengto/tutorialmod/item/ModItems.java
@@ -1,8 +1,12 @@
 package net.bengto.tutorialmod.item;
 
+
 import net.bengto.tutorialmod.TutorialMod;
+import net.bengto.tutorialmod.item.custom.FuelItem;
+import net.bengto.tutorialmod.item.custom.MetalDetectorItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -13,8 +17,15 @@ public class ModItems {
 
     public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
             () -> new Item(new Item.Properties()) );
-    public static  final  RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
+    public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
             () -> new Item(new Item.Properties()));
+    public static  final  RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector",
+            () -> new MetalDetectorItem(new Item.Properties().durability(100)));
+    public static final RegistryObject<Item> STRAWBERRY = ITEMS.register("strawberry",
+            () -> new Item(new Item.Properties().food(ModFoods.STRAWBERRY)));
+    public static final RegistryObject<Item> PINE_CONE = ITEMS.register("pine_cone",
+            () -> new FuelItem(new Item.Properties(),400));
+
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
